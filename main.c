cb513f44ce9963368c32643968ec996050ea9fb6
Frank Ridder <fr.ridder@hotmail.com>
2021-12-14 20:41:56 +0100

    Add 2.1.3

--- main.c
+++ main.c
@@ -13,33 +13,29 @@
 // Terminal variables
 static struct termios term;
 
-#define BILLION 1000000000L;
-
 static pthread_t thread;
 
 static void *taskOne();
 static void *taskTwo();
 static void *taskThree();
+
 static void terminate();
 static void initialiseTerminal();
 static void createThreads();
 static void sighandler(int sig_num);
-int create_timer(unsigned long offs, int period);
-void wait_next_activation(void);
 
-static sigset_t sigset;
+static void create_timer();
 
-int main()
-{
-    
+static sigset_t sset;
+static int sig;
+
+int main() {
     createThreads(); // Create threads
-    
     terminate(); // Wait for threads to end and terminate program
     return 0;
 }
 
-void createThreads()
-{
+void createThreads() {
     long i = 0;
 
     pthread_attr_t tattr;
@@ -49,8 +45,7 @@ void createThreads()
     //int status = pthread_create(&thread, &tattr, taskOne, (void *) i);    //Create threads
     //int status = pthread_create(&thread, &tattr, taskTwo, (void *) i);    //Create threads
     int status = pthread_create(&thread, &tattr, taskThree, (void *)i); //Create threads
-    if (status != 0)
-    {
+    if (status != 0) {
         printf("While creating thread 1, pthread_create returned error code %d\n", status);
         exit(-1);
     }
@@ -58,139 +53,132 @@ void createThreads()
     pthread_exit(NULL);
 }
 
-void *taskOne()
-{
+void *taskOne() {
     int iterations = 10;
     struct timespec time[iterations];
-    for (int i = 0; i < iterations; i++)
-    {
+    for (int i = 0; i < iterations; i++) {
         struct timespec tim, tim2;
         double sum = 0.0;
 
-        for (int i = 0; i < 1e4; i++)
-        {
-            sum += sqrt(i);
+        for (int j = 0; j < 1e4; j++) {
+            sum += sqrt(j);
         }
 
-        if (clock_gettime(CLOCK_REALTIME, &time[i]) == -1)
-        {
+        if (clock_gettime(CLOCK_REALTIME, &time[i]) == -1) {
             perror("clock gettime");
             exit(EXIT_FAILURE);
         }
 
         tim.tv_sec = 0;
         tim.tv_nsec = 1000000L;
-        if (nanosleep(&tim, &tim2) < 0)
-        {
+        if (nanosleep(&tim, &tim2) < 0) {
             printf("Nano sleep system call failed \n");
         }
     }
-    for (int i = 0; i < iterations; i++)
-    {
-        printf("%02ld:%03ld \n", time[i].tv_sec % 100, (long)(time[i].tv_nsec / 1e6));
+    for (int i = 0; i < iterations; i++) {
+        printf("%02ld:%03ld \n", time[i].tv_sec % 100, (long) (time[i].tv_nsec / 1e6));
     }
 }
 
-void *taskTwo()
-{
+void *taskTwo() {
     int iterations = 10;
     struct timespec time[iterations];
-    for (int i = 0; i < iterations; i++)
-    {
+    for (int i = 0; i < iterations; i++) {
         struct timespec deadline;
         clock_gettime(CLOCK_MONOTONIC, &deadline);
         // Add the time you want to sleep
         deadline.tv_nsec += 1000000L;
 
         // Normalize the time to account for the second boundary
-        if (deadline.tv_nsec >= 1000000000)
-        {
+        if (deadline.tv_nsec >= 1000000000) {
             deadline.tv_nsec -= 1000000000;
             deadline.tv_sec++;
         }
 
         double sum = 0.0;
-        for (int i = 0; i < 1e2; i++)
-        {
-            sum += sqrt(i);
+        for (int j = 0; j < 1e2; j++) {
+            sum += sqrt(j);
         }
 
-        if (clock_gettime(CLOCK_MONOTONIC, &time[i]) == -1)
-        {
+        if (clock_gettime(CLOCK_MONOTONIC, &time[i]) == -1) {
             perror("clock gettime");
             exit(EXIT_FAILURE);
         }
 
-        if (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &deadline, NULL) < 0)
-        {
+        if (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &deadline, NULL) < 0) {
             printf("BIgly error");
         }
     }
-    for (int i = 0; i < iterations; i++)
-    {
-        printf("%02ld:%03ld \n", time[i].tv_sec % 100, (long)(time[i].tv_nsec / 1e6));
+    for (int i = 0; i < iterations; i++) {
+        printf("%02ld:%03ld \n", time[i].tv_sec % 100, (long) (time[i].tv_nsec / 1e6));
     }
 }
 
-void wait_next_activation(void)
-{
-    int dummy;
-    sigwait(&sigset, &dummy);
-}
-int create_timer(unsigned long offs, int period)
-{
-    struct itimerspec t;
-    struct sigevent sigev;
+
+void create_timer() {
+    struct sigevent ev;
     timer_t timer;
-    const int signal = SIGALRM;
-    int res;
-    t.it_value.tv_sec = offs / 1000000;
-    t.it_value.tv_nsec = (offs % 1000000) * 1000;
-    t.it_interval.tv_sec = period / 1000000;
-    t.it_interval.tv_nsec = (period % 1000000) * 1000;
-    sigemptyset(&sigset);
-    sigaddset(&sigset, signal);
-    sigprocmask(SIG_BLOCK, &sigset, NULL);
-    memset(&sigev, 0, sizeof(struct sigevent));
-    sigev.sigev_notify = SIGEV_SIGNAL;
-    sigev.sigev_signo = signal;
-    res = timer_create(CLOCK_MONOTONIC, &sigev, &timer);
-    if (res < 0)
-    {
-        return res;
+    struct itimerspec tset;
+
+    /* block SIGUSR1 */
+    if (sigemptyset(&sset) < 0) {
+        printf("sigemptyset() failed");
     }
-    return timer_settime(timer, 0, &t, NULL);
+    if (sigaddset(&sset, SIGUSR1) < 0) {
+        printf("sigaddset() failed");
+    }
+    if (sigprocmask(SIG_BLOCK, &sset, NULL) < 0) {
+        printf("sigprocmask() failed");
+    }
+
+    /* create timer that sends SIGUSR1 on expiration */
+    ev.sigev_notify = SIGEV_SIGNAL;
+    ev.sigev_signo = SIGUSR1;
+    ev.sigev_value.sival_ptr = &timer;
+
+    if (timer_create(CLOCK_MONOTONIC, &ev, &timer) < 0) {
+        printf("timer_create() failed");
+    }
+    /* get current time value */
+    if (clock_gettime(CLOCK_MONOTONIC, &(tset.it_value)) < 0) {
+        printf("clock_gettime() failed");
+    }
+
+    /* arm timer to start after 1s and expire every 1ms */
+    tset.it_value.tv_sec = tset.it_value.tv_sec + 1;
+    tset.it_interval.tv_sec = 0;
+    tset.it_interval.tv_nsec = 1000000L;
+    if (timer_settime(timer, TIMER_ABSTIME, &tset, NULL) < 0) {
+        printf("timer_settime() failed");
+    }
 }
 
-void *taskThree()
-{
+void *taskThree() {
     int iterations = 10;
     struct timespec time[iterations];
-    create_timer(0, 1e3);
-    for (int i = 0; i < iterations; i++)
-    {
-
+    create_timer();
+    for (int i = 0; i < iterations; i++) {
         double sum = 0.0;
-        for (int i = 0; i < 1e2; i++)
-        {
-            sum += sqrt(i);
+        for (int j = 0; j < 1e2; j++) {
+            sum += sqrt(j);
         }
 
-        if (clock_gettime(CLOCK_MONOTONIC, &time[i]) == -1)
-        {
+        if (clock_gettime(CLOCK_MONOTONIC, &time[i]) == -1) {
             perror("clock gettime");
             exit(EXIT_FAILURE);
         }
-        wait_next_activation();
+        /* sleep for another 10s */
+
+        if (sigwait(&sset, &sig)) {
+            printf("failed sigwait()");
+        }
     }
-    for (int i = 0; i < iterations; i++)
-    {
-        printf("%02ld:%03ld \n", time[i].tv_sec % 100, (long)(time[i].tv_nsec / 1e6));
+    for (int i = 0; i < iterations; i++) {
+        printf("%02ld:%03ld \n", time[i].tv_sec % 100, (long) (time[i].tv_nsec / 1e6));
     }
 }
 
-void terminate()
-{
+void terminate() {
     /* Wait for threads to end */
     pthread_join(thread, NULL);
 }
